77fe230b88d628961d9c3dca5aa1c5d337cd5e74
ThienSon Ho <thiensonho@utexas.edu>
2019-10-27 15:04:09 -0500

    Enabled cors on backend. Connected frontend to local server

--- CONNECT_DJANGO/connectbackend/connectbackend/settings.py
+++ CONNECT_DJANGO/connectbackend/connectbackend/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'students',
     'results',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = 'http://localhost:3000',
--- my-app/src/pages/Admin.js
+++ my-app/src/pages/Admin.js
@@ -1,12 +1,73 @@
 import React, { Component } from 'react';
-class Admin extends Component {  
-    render() {
+class Admin extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			students: null,
+			results: null,
+		}
+	}
+
+	componentDidMount() {
+		fetch('http://127.0.0.1:8000/api/students/', {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+				'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
+				'Access-Control-Allow-Credentials' : true ,
+				'Access-Control-Allow-Headers': 'X-Requested-With'
+			},
+		})
+			.then(response => {
+				response.json().then(data => {
+					console.log(data);
+					this.setState({
+						students: data
+					})
+				});
+			})
+			.catch(err => console.log(err));
+
+		fetch('http://127.0.0.1:8000/api/matchings/', {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+				'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
+				'Access-Control-Allow-Credentials' : true ,
+				'Access-Control-Allow-Headers': 'X-Requested-With'
+			},
+		})
+			.then(response => {
+				response.json().then(data => {
+					console.log(data);
+					this.setState({
+						results: data
+					})
+				});
+			})
+			.catch(err => console.log(err));
+	}
+
+	render() {
+		let hasMounted = false;
+		if(this.state.students !== null) hasMounted = true;
+
 		return (
 			<div align="center">
 				<h1>Admin Page</h1>
 				<p> Only those with administration access to the RGK CONNECT Program can login here. </p>
-				
-
+				{hasMounted ? (
+					this.state.students.map((value, index) => {
+						return (
+							<p key={index}>{value.name}</p>
+						);
+					})) : (
+						<p>No data</p>
+					)
+				}
 			</div>
 			
 		);
