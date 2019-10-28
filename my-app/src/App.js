d16eb746a84bc6d19ec096837bc23c7bff6732f7
Arman <armankhondker@utexas.edu>
2019-10-28 11:09:30 -0500

    Major Front End Design Changes
    Added a sticky bar feature at the top of every page, added to about page, added login page for admins, etc.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Student from './pages/Student.js';
 import Admin from './pages/Admin.js';
@@ -7,11 +8,13 @@ import HomeLinks from './pages/HomeLinks.js'
 import Central from './pages/Central.js'
 import CommunityOrg from './pages/CommunityOrg.js'
 import About from './pages/About'
+import StickNavbar from "./pages/StickyNavbar";
 
 class App extends Component {
   render(){
   return (
     <BrowserRouter>
+    <Route path ='/' render={() => (<StickNavbar />)}/>
     <Route exact={true} path='/' render={() => (<Central />)}/>  
     <Route exact={true} path='/home' render={() => (<HomeLinks />)}/>
     <Route exact={true} path='/student' render={() => (<Student />)}/>
--- my-app/src/pages/Admin.js
+++ my-app/src/pages/Admin.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
+import '.././App.css';
+import Form from "react-bootstrap/Form";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import Button from "react-bootstrap/Button"
+
 class Admin extends Component {  
     render() {
 		return (
-			<div align="center">
+			<div align="center" className="App">
 				<h1>Admin Page</h1>
 				<p> Only those with administration access to the RGK CONNECT Program can login here. </p>
-				
+				<Form>
 
+					<Form.Group as={Row} controlId="formPlaintextPassword">
+						<Form.Label column sm="5">
+						Username
+						</Form.Label>
+						<Col sm="15">
+						<Form.Control type="username" placeholder="Username" />
+						</Col>
+					</Form.Group>
+				
+					<Form.Group as={Row} controlId="formPlaintextPassword">
+						<Form.Label column sm="5">
+						Password
+						</Form.Label>
+						<Col sm="15">
+						<Form.Control type="password" placeholder="Password" />
+						</Col>
+					</Form.Group>
+				</Form>
+
+				<p></p>
+				<Button className="CreateAccount" variant="light">Create Account</Button>
+				   <p></p>
+				<Button className="LoginButton" variant="danger">Login</Button>
+
+               
+        
 			</div>
 			
 		);
