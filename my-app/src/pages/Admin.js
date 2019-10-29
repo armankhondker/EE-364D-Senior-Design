f3c9ee8c85d8a50e3736afb25787bdf997a7a4de
ThienSon Ho <thiensonho@utexas.edu>
2019-10-29 01:52:11 -0500

    Fixed cors error by using axios and proxy on frontend. Also added new cors middleware in backend

--- my-app/src/pages/Admin.js
+++ my-app/src/pages/Admin.js
@@ -1,68 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 class Admin extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			students: null,
 			results: null,
 		}
 	}
 
 	componentDidMount() {
-		fetch('http://127.0.0.1:8000/api/students/', {
-			method: 'GET',
-			mode: 'cors',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-				'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
-				'Access-Control-Allow-Credentials' : true ,
-				'Access-Control-Allow-Headers': 'X-Requested-With'
-			},
-		})
-			.then(response => {
-				response.json().then(data => {
-					console.log(data);
-					this.setState({
-						students: data
-					})
-				});
-			})
-			.catch(err => console.log(err));
-
-		fetch('http://127.0.0.1:8000/api/matchings/', {
-			method: 'GET',
-			mode: 'cors',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-				'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
-				'Access-Control-Allow-Credentials' : true ,
-				'Access-Control-Allow-Headers': 'X-Requested-With'
-			},
-		})
-			.then(response => {
-				response.json().then(data => {
-					console.log(data);
-					this.setState({
-						results: data
-					})
-				});
-			})
-			.catch(err => console.log(err));
+        axios.get('http://127.0.0.1:8000/api/matchings')
+			.then(res => {
+				console.log(res);
+				this.setState({ results: res.data });
+			});
 	}
 
 	render() {
 		let hasMounted = false;
-		if(this.state.students !== null) hasMounted = true;
+		if(this.state.results !== null) hasMounted = true;
 
 		return (
 			<div align="center">
 				<h1>Admin Page</h1>
 				<p> Only those with administration access to the RGK CONNECT Program can login here. </p>
 				{hasMounted ? (
-					this.state.students.map((value, index) => {
+					this.state.results.map((value, index) => {
 						return (
-							<p key={index}>{value.name}</p>
+							<p key={index}><strong>{value.student}</strong> -> {value.project_org}</p>
 						);
 					})) : (
 						<p>No data</p>
