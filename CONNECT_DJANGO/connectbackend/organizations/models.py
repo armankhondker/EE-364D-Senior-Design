fc7dcf2f51ccc29ec3b045744731ccacc9fa2ea2
ThienSon Ho <thiensonho@utexas.edu>
2019-11-19 00:50:10 -0600

    Added project model to API. Loaded project data and student data with new scripts.

---
+++ CONNECT_DJANGO/connectbackend/organizations/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    primary = models.CharField(max_length=100)
+    secondary = models.CharField(max_length=100, blank=True)
+    technical = models.DecimalField(decimal_places=3, max_digits=5)
+    professional = models.DecimalField(decimal_places=3, max_digits=5)
+    quadrant = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
--- CONNECT_DJANGO/connectbackend/students/models.py
+++ CONNECT_DJANGO/connectbackend/students/models.py
@@ -3,6 +3,18 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=12)
+    email = models.EmailField(max_length=100, blank=True)
+    phone = models.CharField(max_length=12, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    resume_id = models.CharField(max_length=100)
+    quadrant = models.CharField(max_length=100)
+    technical = models.DecimalField(decimal_places=3, max_digits=5)
+    professional = models.DecimalField(decimal_places=3, max_digits=5)
+    availability_duration = models.CharField(max_length=100, blank=True)
+    availability_time = models.CharField(max_length=100, blank=True)
+    work_factors = models.CharField(max_length=100, blank=True)
+    interest_buckets = models.CharField(max_length=100, blank=True)
+    other = models.TextField(max_length=500, blank=True)
+
+
+
