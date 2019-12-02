5a389e7c5b08e0aa08c404b928e17263742420d5
Arman <armankhondker@utexas.edu>
2019-12-01 23:03:03 -0600

    Updated about page styling

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -13,7 +13,7 @@ class About extends Component {
 				<p>About rgkconnectmatching.com</p>
 				<p>filler</p>
 
-                <h2>Our Development Team:</h2>
+                <h2> Our Development Team:</h2>
 				<ul>
 				<p><b>Arman Khondker</b></p> 
 				{ <img className="prof_pic" src={Arman} alt="Arman" /> }
@@ -22,21 +22,51 @@ class About extends Component {
                <br></br>
                Major: Electrical and Computer Engineering
                <br></br>
-			   Responsibilites: Front End Development Lead, Natural Language Processing of Resumes
+			   Responsibilites: Front End Development Lead
                <br></br>
 					</ul>
 			
                <ul><p><b>Desiree Tang</b></p> 
 			   { <img className="prof_pic" src={Desiree} alt="Desiree" /> }
+			   <br></br>
+               Bio: A senior Computer Engineering student with technical cores in Software Engineering and Academic enrichmnent
+               <br></br>
+               Major: Electrical and Computer Engineering
+               <br></br>
+			   Responsibilites: Project Lead
+               <br></br>
 			   </ul>
 			   <ul><p><b>Punit Patel</b></p>
 				{ <img className="prof_pic" src={Punit} alt="Punit" /> } 
+				<br></br>
+               Bio: A senior Computer Engineering student with technical cores in Software Engineering and Academic enrichmnent
+               <br></br>
+               Major: Electrical and Computer Engineering
+               <br></br>
+			   Responsibilites: Report Lead
+               <br></br>
 				</ul>
+
 				<ul> <p><b>Josh Papermaster</b></p>
 				{ <img className="prof_pic" src={Josh} alt="Josh" /> }
+				<br></br>
+               Bio: A senior Computer Engineering student with technical cores in Software Engineering and Academic enrichmnent
+               <br></br>
+               Major: Electrical and Computer Engineering
+               <br></br>
+			   Responsibilites: Algorithm Lead
+               <br></br>
 				</ul>
-                <ul> <p><b>Thienson Ho</b></p>
+
+             <ul> <p><b>Thienson Ho</b></p>
 				{ <img className="prof_pic" src={ThienSon} alt="ThienSon" /> }
+				<br></br>
+               Bio: A senior Computer Engineering student with technical cores in Software Engineering and Academic enrichmnent
+               <br></br>
+               Major: Electrical and Computer Engineering
+               <br></br>
+			   Responsibilites: Back End Lead
+               <br></br>
 				</ul>
 			</div>
 			
