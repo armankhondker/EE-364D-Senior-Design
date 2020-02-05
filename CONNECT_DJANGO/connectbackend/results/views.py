e66da8d217c68b58d81aadf86501237a7bf3f836
ThienSon Ho <thiensonho@utexas.edu>
2020-02-05 10:02:49 -0600

    Started creating results view.

--- CONNECT_DJANGO/connectbackend/results/views.py
+++ CONNECT_DJANGO/connectbackend/results/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .models import Matching
+from .serializers import MatchingSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+def get(request, format=None):
+    matches = Matching.objects.all()
+    serializer = MatchingSerializer(matches, many=True)
+    return Response(serializer.data)
+
+
+class MatchResults(APIView):
+    """
+    List all matches or create a new matching
+    """
