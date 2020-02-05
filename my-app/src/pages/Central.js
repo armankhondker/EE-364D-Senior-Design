08707649b911eca270d48f9e560ca6bdf9b73224
Arman <armankhondker@utexas.edu>
2020-02-05 17:32:25 -0600

    Removed program description

--- my-app/src/pages/Central.js
+++ my-app/src/pages/Central.js
@@ -16,14 +16,6 @@ class Central extends Component {
 					<br></br>
                     <br></br>
 
-					<div>
-      <h2 ><b>CONNECT Program Overview</b></h2>
-<p class="rteindent4">Central Texas organizations have big challenges to tackle. Graduate students are among the people in our Central Texas community that have the skills organizations need to solve these problems.&nbsp; CONNECT is a way&nbsp;to bring these groups together. Our hope is that this effort will continue to grow and make a contribution toward solving complex community challenges.&nbsp;</p>
-<br></br>
-<p class="rteindent4">CONNECT is a platform focused on connecting organizations in the community who have a data, evaluation, or measurement - related project to tackle with graduate students from the LBJ School of Public Affairs, the RGK Center's Portfolio Program in Nonprofit Studies,and other graduate degree programs. The program matches organizations that have scoped a project with graduate students looking for applied and practical experience in the social and/or public sector.&nbsp; &nbsp;</p>
-<br></br>
-<p class="rteindent4">For additional information, take a look at our FAQs for <strong><a href="https://rgkcenter.org/connect-frequently-asked-questions-community-organizations">community organizations</a></strong> and <strong><a href="https://rgkcenter.org/connect-frequently-asked-questions-graduate-students">graduate students</a></strong>, or view our <strong><a href="https://rgkcenter.org/connect-project-library">project library</a></strong>.</p>
-  </div>
 				</body>
 			</div>
 		);
