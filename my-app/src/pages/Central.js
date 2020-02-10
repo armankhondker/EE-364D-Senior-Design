451cee101b16ea117b1b697f052ba915b7690344
Arman <armankhondker@utexas.edu>
2020-02-09 18:32:09 -0600

    Added buttons to main page and fixed stickybar naming

--- my-app/src/pages/Central.js
+++ my-app/src/pages/Central.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Logo from '../assets/RGB_formal_RGK.png'
-
-
+import ImgButton from "../components/ImgButton";
+import Admin from "../assets/AdminIcon.png"; 
+import Org from "../assets/OrganizationIcon.png";
+import Student from "../assets/StudentIcon.png";
+import '../components/ImgButton.css';
 
 class Central extends Component {
 
@@ -14,7 +17,11 @@ class Central extends Component {
 				 <img  className="logo" src={Logo} alt="Logo" />
 					<br></br>
 					<br></br>
-                    <br></br>
+					<div>
+						<ImgButton image = {Admin} label = "Admin" path ="admin"></ImgButton>
+						<ImgButton image = {Org} label= "Organizations" path="community"></ImgButton>
+						<ImgButton image = {Student} label ="Students" path="student"></ImgButton>
+					</div>
 
 				</body>
 			</div>
