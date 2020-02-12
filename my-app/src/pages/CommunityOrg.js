8bf5450f6f1297ba6ac9ef53ad93488ab4bc9ed4
[Punit <punitpatel@utexas.edu>
2020-02-12 11:36:03 -0600

    Update CommunityOrg.js

--- my-app/src/pages/CommunityOrg.js
+++ my-app/src/pages/CommunityOrg.js
@@ -9,7 +9,7 @@ class CommunityOrg extends Component {
 				<h1>Community Organizations Page</h1>
 				<br></br>
 				<p> <b> Please fill out the following survey if your community organization has a project they would like to have matched. </b> </p>
-				<a href="https://utexas.qualtrics.com/jfe/form/SV_blz2b1nizqI3Ctf">Project Survey</a>
+				<a href="/communityform">Organization Survey</a>
 			</div>
 			
 		);
