3b8b842fb3dd176322af46d8da4afcb989bdc885
Josh Papermaster <jpapermaster@utexas.edu>
2020-04-06 15:05:18 -0500

    Survey form submit button handling + resume upload

--- my-app/src/pages/StudentForm.js
+++ my-app/src/pages/StudentForm.js
@@ -20,7 +20,8 @@ class StudentForm extends Component {
 						lastNameInput: "",
 						emailInput: "",
 						linkedinInput: "",
-						projectCategories: "",
+						resumeInput: "",
+						uploading: false,
 						timeCommit: "",
 						intentionOptions: ["To gain real world experience", "To participate in a paid experience", "To fulfill an academic requirement (i.e. capstone, thesis, dissertation"],
 						interestOptions: ["Research", "Data Collection", "Software Development", "Business Intelligence", "Data Analytics"],
@@ -46,6 +47,7 @@ class StudentForm extends Component {
 				this.handleLastName = this.handleLastName.bind(this);
 				this.handleEmail = this.handleEmail.bind(this);
 				this.handleLinkedin = this.handleLinkedin.bind(this);
+				this.handleResumeUpload = this.handleResumeUpload.bind(this);
 				this.handleTimeCommit = this.handleTimeCommit.bind(this);
 				this.handleIntentions = this.handleIntentions.bind(this);
 				this.handleInterest = this.handleInterest.bind(this);
@@ -56,6 +58,7 @@ class StudentForm extends Component {
 				this.handleGuidanceQuestion = this.handleGuidanceQuestion.bind(this);
 				this.handleRadio = this.handleRadio.bind(this);
 				this.handleExtraSkills = this.handleExtraSkills.bind(this);
+				this.handleSubmit = this.handleSubmit.bind(this);
     }
 		handlePhone(e) {
 			var num = e.target.value;
@@ -90,6 +93,33 @@ class StudentForm extends Component {
 			this.setState(state => ({
 	      lastNameInput: name
 	    }));
+		}
+
+		handleResumeUpload(e) {
+			this.setState({ uploading: true })
+			const files = Array.from(e.target.files)
+			const formData = new FormData()
+
+			this.setState({
+				uploading: false,
+				resumeInput: files[0]
+			})
+			// files.forEach((file, i) => {
+	    //   formData.append(i, file)
+	    // })
+			// fetch(`${API_URL}/image-upload`, {
+	    //   method: 'POST',
+	    //   body: formData
+	    // })
+	    // .then(res => res.json())
+	    // .then(images => {
+	    //   this.setState({
+	    //     uploading: false,
+	    //     images
+	    //   })
+	    // })
+
+
 		}
 
 		handleTimeCommit(e) {
@@ -240,6 +270,40 @@ class StudentForm extends Component {
            .catch(err => console.log(err));
     }
 
+		handleSubmit() {
+			var {skills} = this.state;
+			var {phoneInput} = this.state;
+			var {firstNameInput} = this.state;
+			var {lastNameInput} = this.state;
+			var {emailInput} = this.state;
+			var {linkedinInput} = this.state;
+			var {resumeInput} = this.state;
+
+			var {intentionOptions} = this.state;
+			var {intentionData} = this.state;
+			var {interestOptions} = this.state;
+			var {interestInput} = this.state;
+			var {timeCommit} = this.state;
+
+			var {logisticQuestions} = this.state;
+			var {logisticData} = this.state;
+
+			var {degreeOption} = this.state;
+			var {degreeOtherInput} = this.state;
+
+			var {courseQuestions} = this.state;
+			var {courseInputs} = this.state;
+
+			var {experienceQuestions} = this.state;
+			var {experienceInputs} = this.state;
+
+			var {guidanceSkill} = this.state;
+			var {extraSkills} = this.state;
+
+
+
+		}
+
     render() {
         let hasMounted = false;
         let {skills} = this.state;
@@ -255,22 +319,22 @@ class StudentForm extends Component {
         } else {
             CurrentDisplay =
                 <div className="form">
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <Form.Group controlId="nameInput">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control value={this.state.firstNameInput} onChange={this.handleFirstName} type="text"/>
+                            <Form.Control required value={this.state.firstNameInput} onChange={this.handleFirstName} type="text"/>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control value={this.state.lastNameInput} onChange={this.handleLastName} type="text"/>
+                            <Form.Control required value={this.state.lastNameInput} onChange={this.handleLastName} type="text"/>
                         </Form.Group>
                         <Form.Group controlId="phoneInput">
                             <Form.Label>Phone #</Form.Label>
-                            <Form.Control type="tel" value={this.state.phoneInput} onChange={this.handlePhone} placeholder="5125558888"/>
+                            <Form.Control required type="tel" value={this.state.phoneInput} onChange={this.handlePhone} placeholder="5125558888"/>
                         </Form.Group>
                         <Form.Group controlId="emailInput">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" value={this.state.emailInput} onChange={this.handleEmail} placeholder="example@utexas.edu"/>
+                            <Form.Control required type="email" value={this.state.emailInput} onChange={this.handleEmail} placeholder="example@utexas.edu"/>
                         </Form.Group>
                         <Form.Group controlId="linkedinInput">
                             <Form.Label>LinkedIn (preferred, but not required)</Form.Label>
@@ -278,10 +342,13 @@ class StudentForm extends Component {
                         </Form.Group>
 												<div className="mb-3">
 											    <Form.File id="resumeInput">
-											      <Form.File.Label>Regular file input</Form.File.Label>
-											      <Form.File.Input />
+											      <Form.File.Label>Please upload a PDF of your resume.</Form.File.Label>
+											      <Form.File.Input required accept=".pdf,.PDF" onChange={this.handleResumeUpload}/>
+														{this.state.uploading ? <p>Uploading...</p> : <p></p>}
 											    </Form.File>
 											  </div>
+
+												<br></br>
 
 												<Form.Label>Why are you interested in working on a project? (Check all that apply </Form.Label>
 														{this.state.intentionOptions.map((option, index) => {
@@ -305,7 +372,7 @@ class StudentForm extends Component {
                         <Form.Group controlId="timeCommit">
                             <Form.Label>Realistically, how much time can you commit per week to working on a
                                 project? </Form.Label>
-                            <Form.Control as="select" onChange={this.handleTimeCommit}>
+                            <Form.Control required as="select" onChange={this.handleTimeCommit}>
                                 <option></option>
                                 <option>Less than 5 Hours Per Week</option>
                                 <option>5-10 Hours Per Week</option>
@@ -314,13 +381,15 @@ class StudentForm extends Component {
                             </Form.Control>
                         </Form.Group>
 
+												<br></br>
+
 												{this.state.logisticQuestions.map((question, index) => {
 													if (this.state.logisticFlags[index]) {
 														return (
 															<Form.Group key={index}>
 																	<Form.Label>{question}</Form.Label>
-																	<Form.Check type="Radio" label="Yes" checked={this.state.logisticData[index]} onChange={this.handleLogisticQuestions.bind(this, index, 0)}/>
-																	<Form.Check type="Radio" label="No" checked={!this.state.logisticData[index]} onChange={this.handleLogisticQuestions.bind(this, index, 1)}/>
+																		<Form.Check type="Radio" label="Yes" checked={this.state.logisticData[index]} onChange={this.handleLogisticQuestions.bind(this, index, 0)}/>
+																		<Form.Check type="Radio" label="No" checked={!this.state.logisticData[index]} onChange={this.handleLogisticQuestions.bind(this, index, 1)}/>
 															</Form.Group>
 														)
 													}
@@ -371,7 +440,7 @@ class StudentForm extends Component {
 															return(
 																<Form.Group key={index}>
 																	<Form.Label>{question}</Form.Label>
-																	<Form.Control as="select" onChange={this.handleExperienceQuestions.bind(this,index)}>
+																	<Form.Control required as="select" onChange={this.handleExperienceQuestions.bind(this,index)}>
 																			<option></option>
 																			<option>No Experience</option>
 																			<option>Less than 6 months</option>
@@ -386,18 +455,25 @@ class StudentForm extends Component {
                         <Form.Group controlId="guidanceSkill">
                             <Form.Label>How skilled are you with leading and managing a project from start to finish
                                 with little guidance from your client? </Form.Label>
-                            <Form.Control as="select" onChange={this.handleGuidanceQuestion}>
+                            <Form.Control required as="select" onChange={this.handleGuidanceQuestion}>
                                 <option></option>
-                                <option>1 - Not Skilled at All</option>
+                                <option>1 - No Experience</option>
                                 <option>2</option>
-                                <option>3</option>
+                                <option>3 - Somewhat Skilled</option>
                                 <option>4</option>
-                                <option>5 - Extremely Skilled</option>
+                                <option>5 - Extremely Experienced</option>
                             </Form.Control>
                         </Form.Group>
 
 
-                        <Form.Label>Please Rate your Experience in the following Technical Skills: </Form.Label>
+                        <Form.Label>Please rate your experience in the following technical skills using the scale below:</Form.Label>
+												<br/>
+												<div>1: No Experience </div>
+												<div>2: Slightly Experienced	</div>
+												<div>3: Can Learn	</div>
+												<div>4: Experienced	</div>
+												<div>5: Extremely Experienced </div>
+												<br/>
                         {skills.map((skill, index) => {
                             let formattedSkill = skill.name.replace(/\s+/g, '');
                             return(
@@ -410,7 +486,14 @@ class StudentForm extends Component {
 
 
 
-                        <Form.Label>Please Rate your Experience in the following Professional Skills: </Form.Label>
+												<Form.Label>Please rate your experience in the following professional skills using the scale below:</Form.Label>
+												<br/>
+												<div>1: No Experience </div>
+												<div>2: Slightly Experienced	</div>
+												<div>3: Can Learn	</div>
+												<div>4: Experienced	</div>
+												<div>5: Extremely Experienced </div>
+												<br/>
                         <Form.Group>
                             <Form.Label>Communication</Form.Label>
                                 <RadioButton name="Communication"/>
@@ -439,7 +522,7 @@ class StudentForm extends Component {
                             <Form.Control type="profList" onChange={this.handleExtraSkills}/>
                         </Form.Group>
 
-                        <Button variant="primary" type="button">
+                        <Button variant="primary" type="submit">
                             Submit
                         </Button>
                     </Form>
