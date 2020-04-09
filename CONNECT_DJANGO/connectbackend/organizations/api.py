eff267fb316f9c3ea9466a137965d3c50ebea099
ThienSon Ho <thiensonho@utexas.edu>
2020-04-09 16:03:41 -0500

    Added 9 new models for the survey for dynamic generation

--- CONNECT_DJANGO/connectbackend/organizations/api.py
+++ CONNECT_DJANGO/connectbackend/organizations/api.py
@@ -1,6 +1,8 @@
 from .models import Project, Skill
 from rest_framework import viewsets, permissions
-from .serializers import ProjectSerializer, SkillSerializer
+from .serializers import ProjectSerializer, SkillSerializer, TechSkillSerializer, ProfSkillSerializer,\
+    TechCourseSerializer, ProfCourseSerializer, IntentionSerializer, DegreeSerializer, InterestSerializer,\
+    LogisticSerializer, ExperienceSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -17,3 +19,75 @@ class SkillViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = SkillSerializer
+
+
+class TechSkillViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = TechSkillSerializer
+
+
+class ProfSkillViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ProfSkillSerializer
+
+
+class TechCourseViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = TechCourseSerializer
+
+
+class ProfCourseViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ProfCourseSerializer
+
+
+class IntentionViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = IntentionSerializer
+
+
+class DegreeViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = DegreeSerializer
+
+
+class InterestViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = InterestSerializer
+
+
+class LogisticViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LogisticSerializer
+
+
+class ExperienceViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ExperienceSerializer
--- CONNECT_DJANGO/connectbackend/organizations/serializers.py
+++ CONNECT_DJANGO/connectbackend/organizations/serializers.py
@@ -13,3 +13,59 @@ class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
         fields = '__all__'
+
+
+class TechSkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class ProfSkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class TechCourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class ProfCourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class IntentionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class DegreeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class InterestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class LogisticSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
+class ExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+
