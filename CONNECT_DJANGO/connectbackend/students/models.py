2a32622c49349c254f3a256aeffbde9f9ced032b
thienson-ho <thiensonho@utexas.edu>
2020-04-09 22:18:29 -0500

    Adding migration for the updated student model. It now has array fields. In order to use them we have to add psycog2 and postgres libraries. Edited EB configurations in order to make it deployable on AWS.

--- CONNECT_DJANGO/connectbackend/students/models.py
+++ CONNECT_DJANGO/connectbackend/students/models.py
@@ -1,27 +1,27 @@
-from django.db import models
+# from django.db import models
+from djongo import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, blank=True)
-    phone = models.CharField(max_length=12, blank=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    eid = models.CharField(max_length=20)
+    phone = models.CharField(max_length=12)
+    email = models.EmailField(max_length=100)
+    linkedIn = models.URLField(max_length=100, blank=True)
+    resume_link = models.URLField(max_length=100, blank=True)
+    intentions = ArrayField(models.CharField(max_length=100, blank=True), size=100)
+    interests = ArrayField(models.CharField(max_length=100, blank=True), size=100)
+    time_commitment = models.CharField(max_length=100)
+    logistics = ArrayField(models.CharField(max_length=100), size=100)
+    degree = models.CharField(max_length=100)
+    tech_courses = ArrayField(models.CharField(max_length=100, blank=True), size=100)
+    prof_courses = ArrayField(models.CharField(max_length=100, blank=True), size=100)
+    experience = ArrayField(models.CharField(max_length=100, blank=True), size=100)
+    tech_skills = ArrayField(models.CharField(max_length=100), size=100)
+    prof_skills = ArrayField(models.CharField(max_length=100), size=100)
+    other_skills = models.TextField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    resume_id = models.CharField(max_length=100)
-    quadrant = models.CharField(max_length=100)
-    technical = models.DecimalField(decimal_places=3, max_digits=5)
-    professional = models.DecimalField(decimal_places=3, max_digits=5)
-    availability_duration = models.CharField(max_length=100, blank=True)
-    availability_time = models.CharField(max_length=100, blank=True)
-    work_factors = models.CharField(max_length=100, blank=True)
-    interest_buckets = models.CharField(max_length=100, blank=True)
-    other = models.TextField(max_length=500, blank=True)
 
 
-class Student2(models.Model):
-    first_name = models.CharField(max_length=100, blank=False)
-    last_name = models.CharField(max_length=100, blank=False)
-    eid = models.CharField(max_length=10, blank=False, unique=True)
-    email = models.EmailField(max_length=100, blank=False, unique=True)
-    phone = models.CharField(max_length=20, blank=True)
-    linkedin = models.CharField(max_length=100, blank=True)
-    # TBD
