1cdbd9c54abf2519566c191b2dccf413122b8bcd
Josh Papermaster <jpapermaster@utexas.edu>
2020-04-23 17:16:16 -0500

    Student survey editing progress

--- my-app/src/components/AdminStudents.js
+++ my-app/src/components/AdminStudents.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import Button from "react-bootstrap/Button"
+import Modal from 'react-bootstrap/Modal'
+import ModalHeader from 'react-bootstrap/Modal'
+import ModalBody from 'react-bootstrap/Modal'
 import NewWindowPortal from "../components/NewWindowPortal.js";
 import '../styling/Admin.css';
+import Form from 'react-bootstrap/Form';
+import RadioButton from "./RadioButton";
+import LoadingAnimation from "../components/LoadingAnimation";
+import update from 'react-addons-update';
+import Popup from "reactjs-popup";
+import '../App.css';
+import axios from "axios";
+
 
 class AdminStudents extends Component {
 
@@ -9,27 +20,124 @@ class AdminStudents extends Component {
         super(props);
         this.state = {
             students: props.students,
+            modalShow: [],
             studentClicked: false,
+            firstName: "",
+            lastName: "",
+            eid: "",
+            phone:  "",
+            email: "",
+            linkedin: "",
+
         }
-        this.toggleWindowPortal = this.toggleWindowPortal.bind(this);
+        this.handleModal = this.handleModal.bind(this);
+        this.renderSurvey = this.renderSurvey.bind(this);
+        this.handleFirstName = this.handleFirstName.bind(this);
+        this.handleLastName = this.handleLastName.bind(this);
+        this.handleEID = this.handleEID.bind(this);
+        this.handlePhone = this.handlePhone.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handleLinkedin = this.handleLinkedin.bind(this);
+
     }
 
-    toggleWindowPortal(){
-        this.setState(state => ({
-            ...state,
-            showWindowPortal: !state.showWindowPortal,
-        }));
+    handleFirstName(e) {
+  		var writtenText = e.target.value
+  		this.setState(state => ({
+  			firstName: writtenText
+  		}));
+  	}
+
+    handleLastName(e) {
+  		var writtenText = e.target.value
+  		this.setState(state => ({
+  			lastName: writtenText
+  		}));
+  	}
+
+    handleEID(e) {
+      var writtenText = e.target.value
+  		this.setState(state => ({
+  			eid: writtenText
+  		}));
     }
 
-    closeWindowPortal(){
-        this.setState(state => ({
-            ...state,
-            showWindowPortal: false,
-        }));
+    handlePhone(e) {
+      var writtenText = e.target.value
+  		this.setState(state => ({
+  			phone: writtenText
+  		}));
+    }
+
+    handleEmail(e) {
+      var writtenText = e.target.value
+  		this.setState(state => ({
+  			email: writtenText
+  		}));
+    }
+
+    handleLinkedin(e) {
+      var writtenText = e.target.value
+  		this.setState(state => ({
+  			linkedin: writtenText
+  		}));
+    }
+
+    renderSurvey(student) {
+      let {first_name, last_name, eid, phone, email, linkedIn, intentions, interests, time_commitment, international, fin_aid, transportation, flexible_hours, work_remotely, degree, tech_courses, prof_courses, experience, tech_skills, prof_skills, other_skills, cohort} = student
+      console.log(intentions)
+      return (
+        <div>
+          <div className="bold">(Leave the text entry blank for survey value to remain as is)</div>
+          <p></p>
+          <div>Current first name: {first_name}</div>
+          <Form.Group controlId="FirstName">
+              <Form.Label>Update First Name Here:</Form.Label>
+              <Form.Control type="profList" onChange={this.handleFirstName}/>
+          </Form.Group>
+          <div>Current last name: {last_name}</div>
+          <Form.Group controlId="LastName">
+              <Form.Label>Update Last Name Here:</Form.Label>
+              <Form.Control type="profList" onChange={this.handleLastName}/>
+          </Form.Group>
+          <div>Current EID: {eid}</div>
+          <Form.Group controlId="EID">
+              <Form.Label>Update EID Here:</Form.Label>
+              <Form.Control type="profList" onChange={this.handleEID}/>
+          </Form.Group>
+          <div>Current phone number: {phone}</div>
+          <Form.Group controlId="Phone">
+              <Form.Label>Update Phone Number Here:</Form.Label>
+              <Form.Control type="profList" onChange={this.handlePhone}/>
+          </Form.Group>
+          <div>Current email: {email}</div>
+          <Form.Group controlId="Email">
+              <Form.Label>Update Email Here:</Form.Label>
+              <Form.Control type="profList" onChange={this.handleEmail}/>
+          </Form.Group>
+          <div>Current LinkedIn: {linkedIn}</div>
+          <Form.Group controlId="Email">
+              <Form.Label>Update LinkedIn Here:</Form.Label>
+              <Form.Control type="profList" onChange={this.handleLinkedin}/>
+          </Form.Group>
+        </div>
+      )
     }
 
     componentDidMount() {
-        this.setState({ students: this.props.students });
+        this.setState({ students: this.props.students, modalShow: new Array(this.props.students.length) });
+    }
+
+    handleModal(i, student, e) {
+      console.log(i)
+      this.setState(update(this.state, {
+       modalShow: {
+         [i] : {
+           $set: true
+         }
+       }
+     }));
+
     }
 
     render(){
@@ -46,11 +154,33 @@ class AdminStudents extends Component {
                         {hasMounted ? (
                             this.state.students.map((student, index) => {
                                 return(
-                                    <tr key={index}>
-                                        <td className="admin_cell">
-                                            <Button onClick={this.toggleWindowPortal}>{student.first_name} {student.last_name}</Button>
-                                        </td>
-                                    </tr>
+                                  <tr key={index}>
+                                    <td className="admin_cell">
+                                      <div>
+                                          <Button onClick={this.handleModal.bind(this, index)}>{student.first_name} {student.last_name}</Button>
+                                          <Modal
+                                             size="lg"
+                                             show={this.state.modalShow[index]}
+                                             onHide={() => (this.setState(update(this.state, {
+                                         			modalShow: {
+                                         				[index] : {
+                                         					$set: false
+                                         				}
+                                         			}
+                                       		   })))}
+                                             aria-labelledby="contained-modal-title-vcenter"
+                                             centered
+                                         >
+                                           <Modal.Header closeButton>
+                                             <Modal.Title id="example-custom-modal-styling-title">
+                                               {student.first_name} {student.last_name}'s Survey
+                                             </Modal.Title>
+                                           </Modal.Header>
+                                           <Modal.Body>{this.renderSurvey(student)}</Modal.Body>
+                                          </Modal>
+                                      </div>
+                                    </td>
+                                  </tr>
                                 );
                             })) : (
                             <p>Loading</p>
