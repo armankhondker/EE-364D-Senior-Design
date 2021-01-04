22e9e3b38cbc938a77fa01bb882ce06d7bf9aa71
ThienSon Ho <thiensonho@utexas.edu>
2021-01-04 14:11:36 -0600

    [BUG] (1) 🐞 In the results pane, added questions are not being included in the match results. The added questions are being considered for the match itself, however.

--- my-app/src/components/AdminResults.js
+++ my-app/src/components/AdminResults.js
@@ -5,146 +5,33 @@ import update from "react-addons-update";
 import '../styling/Admin.css';
 
 function displayInfo(match) {
-   // console.log(match);
-    let studentComponent;
-    let projectComponent;
-    let skills;
-
-        if (!(match.student_name === "NO MATCH")) {
-          skills = match.student_skills;
-          studentComponent =  <div>
-              <br></br>
-              <b>{match.student_name}'s generated scores for each skill</b>
-              <br></br>
-              Communication: {skills.Communication}
-              <br></br>
-              Time Management: {skills['Time Management']}
-              <br></br>
-              Decision Making: {skills['Decision Making']}
-              <br></br>
-              Leadership: {skills.Leadership}
-              <br></br>
-              Teamwork: {skills.Teamwork}
-              <br></br>
-              Literature Review: {skills['Time Management']}
-              <br></br>
-              Baseline Data Identification: {skills['Baseline Data Identification']}
-              <br></br>
-              Logic Modeling: {skills['Logic Modeling']}
-              <br></br>
-              Outcomes Definition: {skills['Time Management']}
-              <br></br>
-              Survey Administration: {skills['Survey Administration']}
-              <br></br>
-              Conducting Interviews: {skills['Conducting Interviews']}
-              <br></br>
-              Data Mining: {skills['Data Mining']}
-              <br></br>
-              Statistical Analysis: {skills['Statistical Analysis']}
-              <br></br>
-              SQL: {skills.SQL}
-              <br></br>
-              Machine Learning: {skills['Machine Learning']}
-              <br></br>
-              HTML: {skills.HTML}
-              <br></br>
-              CSS: {skills.CSS}
-              <br></br>
-              Java: {skills.Java}
-              <br></br>
-              Python: {skills.Python}
-              <br></br>
-              Tableau: {skills.Tableau}
-              <br></br>
-              Microsoft Office Suite: {skills['Microsoft Office Suite']}
-              <br></br>
-              <br></br>
-          </div>;
-        }
-        else {
-          // console.log(match)
-          studentComponent =  <div>
-              <br></br>
-              <p>The Possible Matches Were:</p>
-              <br></br>
-              {match.possible_students.map((name, index) => {
-                  return(
-                    <p>{name}</p>
-                  );
-              })}
-
-          </div>
-        }
-
-        skills = match.org_skills;
-
-        projectComponent = <div style={{textAlign: 'left'}}>
-            <br></br>
-            <b>{match.org_name}'s generated scores for each skill</b>
-            <br></br>
-            Communication: {skills.Communication}
-            <br></br>
-            Time Management: {skills['Time Management']}
-            <br></br>
-            Decision Making: {skills['Decision Making']}
-            <br></br>
-            Leadership: {skills.Leadership}
-            <br></br>
-            Teamwork: {skills.Teamwork}
-            <br></br>
-            Literature Review: {skills['Time Management']}
-            <br></br>
-            Baseline Data Identification: {skills['Baseline Data Identification']}
-            <br></br>
-            Logic Modeling: {skills['Logic Modeling']}
-            <br></br>
-            Outcomes Definition: {skills['Time Management']}
-            <br></br>
-            Survey Administration: {skills['Survey Administration']}
-            <br></br>
-            Conducting Interviews: {skills['Conducting Interviews']}
-            <br></br>
-            Data Mining: {skills['Data Mining']}
-            <br></br>
-            Statistical Analysis: {skills['Statistical Analysis']}
-            <br></br>
-            SQL: {skills.SQL}
-            <br></br>
-            Machine Learning: {skills['Machine Learning']}
-            <br></br>
-            HTML: {skills.HTML}
-            <br></br>
-            CSS: {skills.CSS}
-            <br></br>
-            Java: {skills.Java}
-            <br></br>
-            Python: {skills.Python}
-            <br></br>
-            Tableau: {skills.Tableau}
-            <br></br>
-            Microsoft Office Suite: {skills['Microsoft Office Suite']}
-            <br></br>
-            <br></br>
-            </div>;
-
-    if (!(match.student_name === "NO MATCH")) {
-      return <div>
-          <h3>Project: {match.org_name}</h3>
-          {projectComponent}
-          <h3>Student: {match.student_name}</h3>
-          {studentComponent}
-          <h3>Matchability between Project and Student</h3>
-          Matchability: {match.student_matchability}
-      </div>;
-    }
-    else {
-      return <div>
-          <h3>Project: {match.org_name}</h3>
-          {projectComponent}
-          <h3>Student: {match.student_name}</h3>
-          {studentComponent}
-      </div>;
-    }
+    return <div>
+      <h3>Project: {match.org_name}</h3>
+      {Object.keys(match.org_skills).map((skill, score) => {
+          return (
+                  <React.Fragment>
+                      {skill}: {score}
+                      <br/>
+                  </React.Fragment>
+          )
+      })}
+      <br/>
+      <h3>Student: {match.student_name}</h3>
+      {Object.keys(match.student_skills).map((skill, score) => {
+          return (
+              <React.Fragment>
+                  {skill}: {score}
+                  <br/>
+              </React.Fragment>
+          )
+      })}
+      <br/>
+      {(!(match.student_name === "NO MATCH")) ? <div>
+              <h3>Matchability between Project and Student</h3>
+              Matchability: {match.student_matchability.toFixed(4)}
+          </div> : <div/>
+      }
+    </div>;
 }
 
 class AdminResults extends Component {
@@ -165,7 +52,7 @@ class AdminResults extends Component {
     }
 
     clearData() {
-        this.setState(state => ({
+        this.setState(() => ({
             modalShow: [],
         }));
     }
@@ -207,7 +94,7 @@ class AdminResults extends Component {
                         {hasMounted ?
                             value.data.data_list.map((elem,i) => {
                                 return(
-                                    <tr>
+                                    <tr key={i}>
                                         <td className="admin_cell">
                                             <div>
                                                 <Button onClick={this.handleModal.bind(this, i)}>{elem.org_name} -> {elem.student_name}</Button>
