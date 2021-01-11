d716859e05de3f4401353709e8939529f9b52e6c
ThienSon Ho <thiensonho@utexas.edu>
2021-01-11 01:04:50 -0600

    (3) Show multiple matching results

--- my-app/src/components/AdminResults.js
+++ my-app/src/components/AdminResults.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+
 import update from "react-addons-update";
 import '../styling/Admin.css';
-
 function displayInfo(match) {
-    return <div>
-      <h3>Project: {match.org_name}</h3>
-      {Object.keys(match.org_skills).map((skill, score) => {
-          return (
-                  <React.Fragment>
+    if(match === undefined || match === null || match.student_name === "NO MATCH") {
+        return <div>NO MATCH</div>;
+    } else {
+        return <div>
+          <h3>Project: {match.org_name}</h3>
+          {Object.keys(match.org_skills).map((skill, score) => {
+              return (
+                      <React.Fragment key={skill}>
+                          {skill}: {score}
+                          <br/>
+                      </React.Fragment>
+              )
+          })}
+          <br/>
+          <h3>Student: {match.student_name}</h3>
+          {Object.keys(match.student_skills).map((skill, score) => {
+              return (
+                  <React.Fragment key={skill}>
                       {skill}: {score}
                       <br/>
                   </React.Fragment>
-          )
-      })}
-      <br/>
-      <h3>Student: {match.student_name}</h3>
-      {Object.keys(match.student_skills).map((skill, score) => {
-          return (
-              <React.Fragment>
-                  {skill}: {score}
-                  <br/>
-              </React.Fragment>
-          )
-      })}
-      <br/>
-      {(!(match.student_name === "NO MATCH")) ? <div>
-              <h3>Matchability between Project and Student</h3>
-              Matchability: {match.student_matchability.toFixed(4)}
-          </div> : <div/>
-      }
-    </div>;
+              )
+          })}
+          <br/>
+          {(!(match.student_name === "NO MATCH")) ? <div>
+                  <h3>Matchability between Project and Student</h3>
+                  Matchability: {match.student_matchability.toFixed(4)}
+              </div> : <div/>
+          }
+        </div>;
+    }
 }
 
 class AdminResults extends Component {
@@ -85,53 +91,72 @@ class AdminResults extends Component {
             hasMounted = true;
             value = this.props.results[this.props.results.length-1];
         }
-        if(value !== undefined) {
+        if(value !== undefined && hasMounted) {
             return(
                 <div>
                     <p>Click on a matching pair to view scoring of skills</p>
-                    <table style={{width:"50%", margin: "auto"}}>
-                        <tbody className="admin_table">
-                        {hasMounted ?
-                            value.data.data_list.map((elem,i) => {
-                                return(
-                                    <tr key={i}>
-                                        <td className="admin_cell">
-                                            <div>
-                                                <Button onClick={this.handleModal.bind(this, i)}>{elem.org_name} -> {elem.student_name}</Button>
-                                                <Modal
-                                                    size="lg"
-                                                    show={this.state.modalShow[i]}
-                                                    onHide={() => (
-                                                        this.clearData.bind(this),
-                                                            this.setState(update(this.state, {
-                                                                modalShow: {
-                                                                    [i] : {
-                                                                        $set: false
-                                                                    }
-                                                                }
-                                                            })))}
-                                                    aria-labelledby="contained-modal-title-vcenter"
-                                                    centered
-                                                >
-                                                    <Modal.Header closeButton>
-                                                        <Modal.Title id="example-custom-modal-styling-title">
-                                                            Match: {elem.org_name} and {elem.student_name}
-                                                        </Modal.Title>
-                                                    </Modal.Header>
-                                                    <Modal.Body>{displayInfo(elem)}</Modal.Body>
-                                                </Modal>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                )
-                            })
+                    <Accordion>
+                    {results.map((result, index) => {
+                        var localDate = new Date(result.created_at);
+                        return (
+                                <Card>
+                                    <Card.Header>
+                                        <Accordion.Toggle as={Card.Header} variant="link" eventKey={index}>
+                                            {result.cohort} {localDate.toDateString()} {localDate.toLocaleTimeString()}
+                                        </Accordion.Toggle>
+                                    </Card.Header>
+                                    <Accordion.Collapse eventKey={index}>
+                                        <Card.Body>
+                                            <table style={{width:"50%", margin: "auto"}}>
+                                                <tbody className="admin_table">
+                                                {hasMounted ?
+                                                    result.data.data_list.map((elem,i) => {
+                                                        return(
+                                                            <tr key={i}>
+                                                                <td className="admin_cell">
+                                                                    <div>
+                                                                        <Button onClick={this.handleModal.bind(this, i)}>{elem.org_name} -> {elem.student_name}</Button>
+                                                                        <Modal
+                                                                            size="lg"
+                                                                            show={this.state.modalShow[i]}
+                                                                            onHide={() => (
+                                                                                this.clearData.bind(this),
+                                                                                    this.setState(update(this.state, {
+                                                                                        modalShow: {
+                                                                                            [i] : {
+                                                                                                $set: false
+                                                                                            }
+                                                                                        }
+                                                                                    })))}
+                                                                            aria-labelledby="contained-modal-title-vcenter"
+                                                                            centered
+                                                                        >
+                                                                            <Modal.Header closeButton>
+                                                                                <Modal.Title id="example-custom-modal-styling-title">
+                                                                                    Match: {elem.org_name} and {elem.student_name}
+                                                                                </Modal.Title>
+                                                                            </Modal.Header>
+                                                                            <Modal.Body>{displayInfo(elem)}</Modal.Body>
+                                                                        </Modal>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        )
+                                                    })
 
-                            : (
-                                <p>Loading</p>
-                            )
-                        }
-                        </tbody>
-                    </table>
+                                                    : (
+                                                        <p>Loading</p>
+                                                    )
+                                                }
+                                                </tbody>
+                                            </table>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                        )
+                    })}
+                    </Accordion>
+
                 </div>
             );
         } else {
